29aad66bffe472775debe252fe533aa635dd74ae
PriyanshuChourasia <105114609+PriyanshuChourasia@users.noreply.github.com>
2023-02-07 16:01:09 +0530

    branch

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import fs from "fs";
+const host = "school-app.local";
 
 export default defineConfig({
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: fs.readFileSync(`D:/laragon/etc/ssl/laragon.key`),
+            cert: fs.readFileSync(`D:/laragon/etc/ssl/laragon.crt`),
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
     ],
 });
